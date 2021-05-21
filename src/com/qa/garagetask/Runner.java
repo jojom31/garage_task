4cc940ae0ced426135c57abae9089e0d25be70d0
JoJo Myles <joneshiamyles31@gmail.com>
2021-05-21 08:59:52 -0400

    practicing objects and classes

---
+++ src/com/qa/garagetask/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.garagetask;
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Vehicle car1 = new Vehicle("car","Honda", "Accord", 2017);
+		System.out.println(car1.make);
+
+	}
+
+}
--- src/com/qa/garagetask/Vehicle.java
+++ src/com/qa/garagetask/Vehicle.java
@@ -1,5 +1,18 @@
 package com.qa.garagetask;
 
 public class Vehicle {
+	
+protected String type;
+protected String make;
+protected String model;
+protected int year;
 
+public Vehicle(String type, String make, String model, int year) {
+	this.type = type;
+	this.make = make;
+	this.model = model;
+	this.year = year;
 }
+} 
+
+

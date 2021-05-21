fc93de3070841e086bfb53252b6f9ad8df3794e8
JoJo Myles <joneshiamyles31@gmail.com>
2021-05-21 15:22:25 -0400

    worked on car and vehicle classes.

--- src/com/qa/garagetask/Vehicle.java
+++ src/com/qa/garagetask/Vehicle.java
@@ -1,18 +1,49 @@
 package com.qa.garagetask;
 
-public class Vehicle {
-	
-protected String type;
-protected String make;
-protected String model;
-protected int year;
+public abstract class Vehicle {
 
-public Vehicle(String type, String make, String model, int year) {
-	this.type = type;
-	this.make = make;
-	this.model = model;
-	this.year = year;
+	private String make;
+	private String model;
+	private int year;
+	private double price;
+
+	public Vehicle(String make, String model, int year, double price) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.price = price;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 }
-} 
-
-
